af7711c6b817df805b83fbeb618c808a016430aa
Nat Moore <natalie@natnat.xyz>
2021-07-29 00:19:08 +0100

    add time interval for lrrp

--- src/TrboDataSvc/lrrp.py
+++ src/TrboDataSvc/lrrp.py
@@ -31,9 +31,9 @@ pt+acc+spd+d09 06 23 01 50 62 57 33
 period:
 1m point+time: 09 06 23 01 52 34 31 3c
 2m point+time: 09 06 23 01 52 34 31 78
-3m point+time: 09 06 23 01 52 34 31 81 34   b4
-4m point+time: 09 06 23 01 52 34 31 81 70   f0
-5m point+time: 09 06 23 01 52 34 31 82 2c   12c
+3m point+time: 09 07 23 01 52 34 31 81 34   b4
+4m point+time: 09 07 23 01 52 34 31 81 70   f0
+5m point+time: 09 07 23 01 52 34 31 82 2c   12c
 '''
 class LRRP():
     LRRP_SP = 1
@@ -111,6 +111,37 @@ class LRRP():
             num = ~num ^ 0xFFFFFFFF
         lon = num * (360.0 / 0xFFFFFFFF)
         return round(lon, 6)
+
+    def sendIntervalRequest(self, rid, seconds):
+        """Sends a request to the radio to send its location every n seconds."""
+
+        # request goes like 09 07 23 01 52 34 31 81 34
+        #the last byte or two are the seconds of the interval, i.e. wait this many seconds before sending loc
+        #31 is the opbyte meaning time interval - this many seconds
+        #one byte after that means that many seconds as decimal - 3c is 60s
+        #two bytes gets interesting though
+        #81 34 means 80 + 34 = b4
+        #82 2c means 80 + 80 + 2c = 01 2c
+        divisions = 0
+        while seconds > 128:
+            seconds = seconds - 128
+            divisions += 1
+
+        multiplierBytes = int(128 + divisions).to_bytes(1, "big")
+        secondBytes = int(seconds).to_bytes(1, "big")
+
+        if (multiplierBytes == b'\x80'):
+            intervalBytes = secondBytes
+        else:
+            intervalBytes = multiplierBytes + secondBytes
+        
+        requestBody = b'\x23\x01\x52\x34\x31' + intervalBytes
+        #get the length of the request and send it
+        length = len(requestBody)
+        length = length.to_bytes(1, "big")
+        request = b'\x09' + length + requestBody
+        ip = util.id2ip(self._cai, rid)
+        self._sock.sendto(request, (ip, self._port))
 
     def sendImmediateRequest(self, rid):
         request = b'\x09\x01\x33'
